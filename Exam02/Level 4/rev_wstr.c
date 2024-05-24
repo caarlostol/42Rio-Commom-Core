f9fd3b538dff62711e8842b99f5746a1e92c672c
TheNbk <caarlostol@gmail.com>
2024-05-24 20:08:05 -0300

    rev_wstr update

--- Exam02/Level 4/rev_wstr.c
+++ Exam02/Level 4/rev_wstr.c
@@ -1,34 +1,40 @@
 #include <unistd.h>
 
-int main(int argc, char **argv)
+int	ft_isblank(char c)
+{
+	if (c == ' ' || c == '\t')
+		return (1);
+	return (0);
+}
+
+int main(int argc, char **av)
 {
 	int start;
 	int end;
 	int i = 0;
+	char	*str = av[1];
 		
-	if(argc == 2)
+	if (argc == 2)
 	{   
-		while(argv[1][i] != '\0')
+		while (str[i])
 			i++;
-		while(i >= 0)
+		while (i >= 0)
 		{
-			while( argv[1][i] == '\0' || argv[1][i] == ' ' || argv[1][i] == '\t')
+			while (str[i] == '\0' || ft_isblank(str[i]))
 				i--;
 			end = i;
-			while(argv[1][i] && argv[1][i] != ' ' && argv[1][i] != '\t')
+			while (str[i] && !ft_isblank(str[i]))
 				i--;
 			start = i + 1;
 			int  flag;
 			flag = start;
-			while(start <= end)
+			while (start <= end)
 			{
-				write (1, &argv[1][start],1);
+				write (1, &str[start],1);
 				start++;		
 			}
-			if (flag !=0)
-			{
+			if (flag)
 				write(1, " ", 1);
-			}
 		}
 	}
 	write(1, "\n", 1);
